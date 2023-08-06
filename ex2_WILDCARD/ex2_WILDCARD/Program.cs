abfb37f2087eb369098bc7865b0bc79d6f08cc17
rlatjrqjaLaptop <rlatjrqja@yuhan.ac.kr>
2023-08-07 02:31:39 +0900

    2주차 와일드카드 문제 시작

---
+++ ex2_WILDCARD/ex2_WILDCARD/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("와일드카드 패턴 : ");
+string wild = Console.ReadLine();
+
+Console.Write("대조될 단어 수 : ");
+int num = Console.Read();
+Console.Write("\n");
+
+string[] text = new string[num];
+for(int i=0; i < num; i++)
+{
+    text[i] = Console.ReadLine();
+}
+
+string a = Console.ReadLine();
+Console.WriteLine(a);
+
+char match()
+{
+    return 'a';
+}
