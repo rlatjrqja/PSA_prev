7cab5cc68b09fae40b102c4b7ca64e5814adcfd8
rlatjrqjaUniv <rlatjrqja@yuhan.ac.kr>
2023-08-07 19:43:15 +0900

    와일드카드 문제 1차 저장

--- ex2_WILDCARD/ex2_WILDCARD/Program.cs
+++ ex2_WILDCARD/ex2_WILDCARD/Program.cs
@@ -2,19 +2,40 @@
 string wild = Console.ReadLine();
 
 Console.Write("대조될 단어 수 : ");
-int num = Console.Read();
+int num = int.Parse(Console.ReadLine());
 Console.Write("\n");
 
-string[] text = new string[num];
+string[] textGroup = new string[num];
 for (int i = 0; i < num; i++)
 {
-    text[i] = Console.ReadLine();
+    textGroup[i] = Console.ReadLine();
 }
 
-string a = Console.ReadLine();
-Console.WriteLine(a);
-
-char match()
+//int index = 0;
+for(int i=0;i< textGroup.Length;i++)
 {
-    return 'a';
+    match(textGroup[i],0);
+}
+
+string match(string text,int index)
+{
+    string ret = "";
+    char c = text[index];
+
+    for(int i=0;i<wild.Length;i++)
+    {
+        if (c == '*' || c == '?')
+        {
+            match(text, ++index);
+        }
+        else if (c == wild[i])
+        {
+            index++;
+            ret += c;
+        }
+        else break;
+    }
+    Console.WriteLine(c);
+
+    return ret;
 }
