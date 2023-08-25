2770a4b8db7040cf2d54357c20b763ef63ce36d1
rlatjrqja <0sogogi0@naver.com>
2023-08-25 16:41:21 +0900

    4ÁÖÂ÷ ¹ßǥ ³¡

--- ex4_RESTORE_algospot/ex4_RESTORE_algospot/FileName.cpp
+++ ex4_RESTORE_algospot/ex4_RESTORE_algospot/FileName.cpp
@@ -16,7 +16,7 @@ int restore(int last, int used)
 	if (res != -1) return res;
 
 	res = 0;
-	for (int i = 0; i < K; i++)
+	for (int i = 0; i < K; i++)                                                                    //i는 i번째 조각을 의미하나?
 	{
 		if ((used & (1 << i)) == 0)
 		{
@@ -27,17 +27,19 @@ int restore(int last, int used)
 	return res;
 }
 
-string reconstruct(int last, int used)
+string reconstruct(int last, int used)                                                                    //(K,0)으로 시작
 {
 	if (used == (1 << K) - 1) return "";
 	for (int next = 0; next < K; next++)
 	{
 		if (used & (1 << next)) continue;
+
 		int ifUsed = restore(next, used + (1 << next)) + over[last][next];
 
 		if (restore(last, used) == ifUsed)
 		{
-			return (word[next].substr(over[last][next]) + reconstruct(next, used + (1 << next)));
+			cout << word[next].substr(over[last][next]) << endl;
+			return (word[next].substr(over[last][next])	+ reconstruct(next, used + (1 << next)));
 		}
 	}
 }
@@ -46,7 +48,7 @@ int overlap(string& s1, string& s2)
 {
 	for (int len = min(s1.size(), s2.size()); len > 0; len--)
 	{
-		if (s1.substr(s1.size() - len) == s2.substr(0, len))
+		if (s1.substr(s1.size() - len) == s2.substr(0, len))                     //뒤부터 n글자, 앞에서 n글자 같은지
 			return len;
 	}
 	return 0;
@@ -74,7 +76,7 @@ int main()
 			{
 				for (int j = 0; j < K; j++)
 				{
-					if (i != j && word[i].find(word[j]) != -1)
+					if (i != j && word[i].find(word[j]) != -1)                                     //word[i]가 다른 문자 조각을 포함하고 있는지
 					{
 						word[j] = word[K - 1];
 						K--;
@@ -82,7 +84,7 @@ int main()
 					}
 				}
 			}
-			if (!removed) break;
+			if (!removed) break;															//배열 전체를 돈 이후 삭제 된 단어가 없으면 탈출
 		}
 		word[K] = "";
 
@@ -90,7 +92,7 @@ int main()
 		{
 			for (int j = 0; j <= K; j++)
 			{
-				over[i][j] = overlap(word[i], word[j]);
+				over[i][j] = overlap(word[i], word[j]);                                        //i번째, j번째 단어는 n개 글자가 겹친다
 			}
 		}
 		cout << reconstruct(K, 0) << endl;
@@ -98,3 +100,4 @@ int main()
 }
 
 //https://chosh95.tistory.com/33
+//https://sangdo913.tistory.com/77
